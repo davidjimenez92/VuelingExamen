bb9b24ee76e4c45401d53a44dbf770de8abc0199
daviiid20 <davidjimenez92@gmail.com>
2022-04-19 10:25:34 +0200

    Added repository integration tests

---
+++ VuelingExamen.Infrastructure.RepositoriesIntegrationTests/AutofacModules/InfrastructureModuleTest.cs
@@ -0,0 +1,23 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VuelingExamen.Domain.Entities;
+using VuelingExamen.Infrastructure.Repositories.Contracts;
+using VuelingExamen.Infrastructure.Repositories.Implementations;
+
+namespace VuelingExamen.Infrastructure.RepositoriesIntegrationTests.AutofacModules
+{
+    public class InfrastructureModuleTest: Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<TextFileRepository>()
+                .As<IFileRepository<Registry>>();
+
+            base.Load(builder);
+        }
+    }
+}
