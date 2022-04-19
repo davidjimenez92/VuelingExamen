820c4139780d11a72ac79f91e3d8270828d2d8ef
daviiid20 <davidjimenez92@gmail.com>
2022-04-19 11:46:02 +0200

    Added Web service contracts and errors

---
+++ VuelingExamen.Distributed.WebServices/Errors/GlobalErrorHandler.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VuelingExamen.Distributed.WebServices.Errors
+{
+    public class GlobalErrorHandler
+    {
+    }
+}
