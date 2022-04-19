5c3e694c8e8f96e08610f23fd8be64018363cbbf
daviiid20 <davidjimenez92@gmail.com>
2022-04-19 11:17:35 +0200

    Added service integration tests

---
+++ VuelingExamen.Application.ServicesIntegrationTests/Implementations/VuelingRegisterServiceUnitTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VuelingExamen.Test.Framework.Utils;
+using VuelingExamen.Application.ServicesIntegrationTests.AutofacModule;
+using VuelingExamen.Application.Dtos;
+using VuelingExamen.Application.Services.Contracts;
+using VuelingExamen.CrossCutting.ProjectConfiguration;
+
+namespace VuelingExamen.Application.ServicesIntegrationTests.Implementations
+{
+    [TestClass()]
+    public class VuelingRegisterServiceUnitTests: IoCSupportedTest<ServicesModuleTest>
+    {
+        private IVuelingService<RegistryDto> _service;
+        private RegistryDto _registry;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _service = Resolve<IVuelingService<RegistryDto>>();
+            _registry = new RegistryDto()
+            {
+                Rebeled = "David",
+                Planet = "Tierra"
+            };
+        }
+        [TestMethod()]
+        public void AddTest()
+        {
+            Assert.IsTrue(_service.Add(_registry, FileType.Text));
+        }
+    }
+}
