a99e7bdd7c62e9f3987f8fcc71ff2d85d3d1fcf3
daviiid20 <davidjimenez92@gmail.com>
2022-04-19 09:10:17 +0200

    first commit

---
+++ VuelingExamen.Distributed.WebServices/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="VuelingExamen.Distributed.WebServices.Global" Language="C#" %>
---
+++ VuelingExamen.Test.Framework.Utils/IoCSupportedTest.cs
@@ -0,0 +1,32 @@
+﻿using Autofac;
+using Autofac.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VuelingExamen.Test.Framework.Utils
+{
+	public class IoCSupportedTest<TModule> where TModule :
+		 IModule, new()
+	{
+		private readonly IContainer container;
+
+		public IoCSupportedTest()
+		{
+			var builder = new ContainerBuilder();
+			builder.RegisterModule(new TModule());
+
+			container = builder.Build();
+		}
+		protected TEntity Resolve<TEntity>()
+		{
+			return container.Resolve<TEntity>();
+		}
+		protected void ShutDownIoc()
+		{
+			container.Dispose();
+		}
+	}
+}
