733bf80f08515a55c989933ea3f03d1ac0ce8527
daviiid20 <davidjimenez92@gmail.com>
2022-04-19 10:00:51 +0200

    Added Repository Unit Tests close #10

---
+++ VuelingExamen.Infrastructure.RepositoriesUnitTests/Implementations/TextFileRepositoryUnitTests.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VuelingExamen.Infrastructure.Repositories.Implementations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VuelingExamen.Domain.Entities;
+using Autofac.Extras.Moq;
+using VuelingExamen.Infrastructure.Repositories.Contracts;
+
+namespace VuelingExamen.Infrastructure.RepositoriesUnitTests.Implementations
+{
+    [TestClass()]
+    public class TextFileRepositoryUnitTests
+    {
+        private Registry registryToSave;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            registryToSave = new Registry()
+            {
+                Rebeld = "David",
+                Planet = "Tierra",
+                RegisterDate = DateTime.Now
+            };
+        }
+
+        [TestMethod()]
+        public void AddTest()
+        {
+            using(var mock = AutoMock.GetLoose())
+            {
+                mock.Mock<IFileRepository<Registry>>()
+                    .Setup(repository => repository.Add(registryToSave))
+                    .Returns(true);
+
+                var mockedRepository = mock.Create<IFileRepository<Registry>>();
+
+                var expectedResult = mockedRepository.Add(registryToSave); 
+
+                mock.Mock<IFileRepository<Registry>>()
+                    .Verify(repository => repository.Add(registryToSave));
+
+                Assert.IsTrue(expectedResult);
+            }
+        }
+    }
+}
